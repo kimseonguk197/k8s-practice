5b059df661de958e71cd93019ddd96ab67a72505
kimseonguk197 <ksg39412@naver.com>
2025-08-25 16:42:26 +0900

    fix

---
+++ 3.msa/ordering/src/main/java/com/beyond/ordersystem/ordering/feignclient/ProductFeignClient.java
@@ -0,0 +1,19 @@
+package com.beyond.ordersystem.ordering.feignclient;
+
+import com.beyond.ordersystem.common.dto.CommonDto;
+import com.beyond.ordersystem.ordering.dto.OrderCreateDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+//name부분은 eureka에 등록된 application.name을 의미
+@FeignClient(name = "product-service")
+public interface ProductFeignClient {
+    @GetMapping("/product/detail/{productId}")
+    CommonDto getProductById(@PathVariable("productId") Long productId);
+
+    @PutMapping("/product/updatestock")
+    void updateProductStockQuantity(@RequestBody OrderCreateDto dto);
+}
